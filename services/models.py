50c316a6343b213a580ad950c3b99b01b207528c
Zack Plauche <zackplauche@gmail.com>
2019-11-05 19:26:47 +0400

    Remove unneeded names for date fields

--- services/models.py
+++ services/models.py
@@ -8,8 +8,8 @@ class Service(models.Model):
     title = models.CharField(max_length=100, unique=True)
     short_description = models.TextField(max_length=500, null=True, help_text='Max characters: 500')
     long_description = models.TextField(blank=True)
-    created_date = models.DateTimeField('date created', default=timezone.now)
-    published_date = models.DateTimeField('date published', null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(null=True, blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=10, default=0.00, blank=True)
 
     @property
