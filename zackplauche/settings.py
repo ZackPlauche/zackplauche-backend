79cdcd3e83a7013be3a72eb96edc5f0ace390858
Zack Plauché <zackknowspython@gmail.com>
2019-12-17 01:09:07 +0400

    Heroku shit

--- zackplauche/settings.py
+++ zackplauche/settings.py
@@ -121,13 +121,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, "zackplauche/staticfiles")
+STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "zackplauche/media")
+MEDIA_URL = '/media/'
 
 django_heroku.settings(locals())
 
-# STATIC_ROOT = os.path.join(BASE_DIR, "zackplauche/staticfiles")
-# STATIC_URL = '/static/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, "zackplauche/media")
-# MEDIA_URL = '/media/'
+
 
 # Email Integration - Mailchimp
 # MAILCHIMP_API_KEY = '479ec0da254fc77e708984c3325f452a-us4'
