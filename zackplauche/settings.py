8a8d04eb73cd9514c2d8bf04321ae643aa3ac6a0
Zack Plauché <zackknowspython@gmail.com>
2020-04-23 19:20:20 +0300

    Finally fix aws heroku shit

--- zackplauche/settings.py
+++ zackplauche/settings.py
@@ -119,7 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "zackplauche/staticfiles")
+STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "zackplauche/media")
 MEDIA_URL = '/media/'
 
@@ -138,17 +139,17 @@ AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-MEDIA_URL = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+#MEDIA_URL = f'http://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 
 AWS_LOCATION = 'static'
-STATIC_ROOT = os.path.join(BASE_DIR, "zackplauche/staticfiles")
-STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
