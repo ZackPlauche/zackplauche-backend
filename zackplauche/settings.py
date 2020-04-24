5480d65b94889deb56b185d3f86b4d5e55b98e98
Zack Plauché <zackknowspython@gmail.com>
2020-04-24 13:06:11 +0300

    Fix DEBUG setting in heroku (take away DEBUG env variable in heroku since it doesn't work)

--- zackplauche/settings.py
+++ zackplauche/settings.py
@@ -15,7 +15,10 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DEBUG']
+if os.environ['DEBUG']:
+    DEBUG = os.environ['DEBUG']
+else:
+    DEBUG = False
 
 ALLOWED_HOSTS = [
     'www.zackplauche.com',
