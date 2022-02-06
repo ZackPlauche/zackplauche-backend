640f902efd1bacdac2991de3aa263a30a90f08b8
Zack Plauché <zackknowspython@gmail.com>
2022-02-06 21:58:50 +0200

    Update production settings with server logging and update python version

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -18,6 +18,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://zackplauche.com/',
     'http://zackplauche.com/',
     'http://www.zackplauche.com/',
+    'http://api.zackplauche.com/',
 ]
 
 # AWS S3 Storage
@@ -28,7 +29,7 @@ AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
 
 AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com' # TODO Fill this in.
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
 ACL_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
@@ -45,6 +46,37 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+
+# Server Logging
+
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt' : "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'MYAPP': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+        },
+    }
+}
+
 
 django_on_heroku.settings(locals())
 del DATABASES['default']['OPTIONS']['sslmode']
